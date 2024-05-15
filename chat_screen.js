a6f6028d5a79a77b457799e35688d966b3860603
xahmad-ali <ahmadhashmi152003@gmail.com>
2024-05-15 22:32:30 +0500

    chat app implemented succesfully

--- chat_screen.js
+++ chat_screen.js
@@ -1,53 +1,120 @@
-import { View,Text,TouchableOpacity,StyleSheet } from 'react-native'
-import React, { useState, useCallback, useEffect } from 'react'
-import { Bubble, GiftedChat } from 'react-native-gifted-chat'
+import React, { useState, useEffect,useCallback } from "react";
+import { Bubble, GiftedChat } from "react-native-gifted-chat";
+import { useRoute } from "@react-navigation/native";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  addDoc,
+  orderBy,
+  onSnapshot,
+} from "firebase/firestore";
+import { db } from "./fireBase.js"; // Assuming you have already set up Firebase in "fireBase.js"
 
-function Chat_screen({navigation,route}) {
-  const [messages, setMessages] = useState([])
-  const {email} =route.params;
+function Chat_screen() {
+  const route = useRoute();
+  const [messages, setMessages] = useState([]);
 
 
 
   useEffect(() => {
-    console.log(email)
-    setMessages([
-      {
-        _id: 1,
-        text: 'Hello',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React Native',
-        },
-      },
-    ])
-  }, [])
-
-  const onSend =messageArray => {
-    setMessages(previousMessages =>
-      GiftedChat.append(previousMessages, messageArray),
-    )
-  }
+    let unsubscribe; // Declare unsubscribe variable outside the try block
   
+    
+        const q = query(
+          collection(db, 'chats', "" + route.params.senderId + route.params.reciverId, 'messages'),
+          orderBy('createdAt', 'desc')
+        );
+  
+        // Subscribe to real-time updates using onSnapshot
+        unsubscribe = onSnapshot(q, (querySnapshot) => {
+          const allMessages = querySnapshot.docs.map(doc => {
+            return { ...doc.data(), createdAt: Date.parse(new Date()) }
+          });  
+
+          setMessages(allMessages);
+        
+        
+        });
+        
+    
+  return()=> unsubscribe();
+   
+  }, []);
+  
+  ////////////////////
+  const onSend = useCallback(async(messageArray = []) => {
+    console.log(messageArray);
+    const msg = messageArray[0];
+    const Mymsg = {
+      ...msg,
+      sendBy: route.params.senderId,
+      reciverId: route.params.reciverId,
+      createdAt: Date.parse(msg.createdAt)
+    };
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, Mymsg)
+    );
+
+    console.log(Mymsg);
+
+    try {
+      await addDoc(
+        collection(
+          db,
+          "chats",
+          "" + route.params.senderId + route.params.reciverId,
+          "messages"
+        ),
+        {
+          ...Mymsg
+        }
+      );
+
+      await addDoc(
+        collection(
+          db,
+          "chats",
+          "" + route.params.reciverId + route.params.senderId,
+          "messages"
+        ),
+        {
+          ...Mymsg
+        }
+        
+      );
+    } catch (error) {
+      console.error("Error sending message:", error);
+    }
+  }, []);
+
+ 
+
+
+
+  ////////////////////////////////////////////////
+  const renderBubble = (props) => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{ right: { backgroundColor: "pink" } }}
+      />
+    );
+  };
+
+  /////////////////////////
   return (
-    <GiftedChat style={{flex:1,backgroundColor:'yellow'}}
-    messages={messages}
-    onSend={messages => onSend(messages)}
-    user={{
-      _id: 1,
-    }}
-    renderBubble={prpos=>{
-      return(
-        <Bubble
-        {...prpos}
-        wrapperStyle={{right:{backgroundColor:'pink'}}}
-        />
-      )
-    }}
-  />
-
-
-  )
+    <GiftedChat
+      style={{ flex: 1, backgroundColor: "yellow" }}
+      messages={messages}
+      onSend={onSend}
+      user={{
+        _id: route.params.senderId,
+      }}
+      renderBubble={renderBubble}
+    />
+  );
 }
 
-export {Chat_screen};
+export { Chat_screen };
--- home.js
+++ home.js
@@ -8,52 +8,64 @@ import {
 } from "react-native";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from "./fireBase";
-import { getAuth } from "firebase/auth";
 import { auth } from "./fireBase";
 
-function Home({ navigation, route }) {
-  const [users, setUsers] = useState();
-  // const {user}= route.params;
 
-  const get_currentUser = async () => {
-    return new Promise((resolve, reject) => {
-      const currentUser = auth.currentUser;
-      console.log("i amfirst",currentUser)
-      if (currentUser) {
-        console.log("asdasd", currentUser.uid)
-        
-        resolve(currentUser.uid)// Resolve with the UID of the current user
-       // return(currentUser.uid)
-      } else {
-        reject(new Error("No user is logged in")); // Reject with an error if no user is logged in
-      }
-    });
-  };
+function Home({ navigation }) {
+  const [users, setUsers] = useState();
+  const [senderId,setSenderId] = useState();
+  // const {user}= route.params;
+  
+
+ // Function to get the current user's ID
+const get_currentUser = async () => {
+  return new Promise((resolve, reject) => {
+    const currentUser = auth.currentUser;
+    if (currentUser) {
+      console.log("from getUser:->",currentUser.email)
+      console.log("Current user id->",currentUser.uid)
+      resolve(currentUser.email); // Resolve with the UID of the current user
+    } else {
+      reject(new Error("No user is logged in")); // Reject with an error if no user is logged in
+    }
+  })
+};
   //////////////////////////////
   const fetchUsers = async () => {
     try {
       // Get the current user ID
-      const currentUserId = await get_currentUser();
-      console.log("next :" ,currentUserId)
-      //currentUserId= currentUserId
-      //console.log("next 2:" ,currentUserId)
-  
+      const currentUserEmail = await get_currentUser();
+      console.log("Current Usser email:", currentUserEmail);
+    
       // Fetch users from Firestore
       const q = query(collection(db, "users"));
       const querySnapshot = await getDocs(q);
   
       let data = [];
       querySnapshot.forEach((doc) => {
-        // Assuming each document has an "email" field
+        // Assuming each document has an "id" field representing the UID
         data.push({ id: doc.id, Email: doc.data().Email });
-        console.log(data)
+      });
+      console.log("Before filter ",data)
+      // Filter out the current user
+      
+     // data = data.filter((user) => user.Email !== currentUserEmail);
+      // Filter out the current user and capture the filtered out user's ID
+      data = data.filter((user) => {
+        if (user.Email === currentUserEmail) {
+          filteredOutUserId = user.id;
+          return false;
+        }
+        return true;
       });
   
-      // Filter out the current user
-      data = data.filter((i) => i.id !== currentUserId);
-      console.log("myname is chiki",data)
+      setSenderId(filteredOutUserId)
+      console.log("Filtered out user ID:", filteredOutUserId);
   
-      // Set the users data
+      // Log filtered users
+      console.log("Filtered Users:", data);
+  
+      // Set the users data (assuming you have a function setUsers to do this)
       setUsers(data);
     } catch (error) {
       console.error("Error fetching users:", error.message);
@@ -65,9 +77,9 @@ function Home({ navigation, route }) {
     fetchUsers();
   }, []);
 
-  const chat = (email) => {
-    console.log(" go to chat ", { email });
-    navigation.navigate("Chat_screen", { email });
+  const chat = (Remail,Rid,senderId) => {
+    console.log(" go to chat ", { Remail,Rid,senderId, });
+    navigation.navigate("Chat_screen", {rEmail: Remail,reciverId:Rid,senderId:senderId });
   };
 
   return (
@@ -87,7 +99,7 @@ function Home({ navigation, route }) {
           data={users}
           renderItem={({ item }) => (
             <View style={{ padding: 15, borderColor: "black" }}>
-              <TouchableOpacity onPress={() => chat(item.Email)}>
+              <TouchableOpacity onPress={() => chat(item.Email,item.id,senderId)}>
                 <Text style={{ fontSize: 30 }}>{item.Email}</Text>
               </TouchableOpacity>
             </View>
